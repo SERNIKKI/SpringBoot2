66031025280f5778502b39ce563279ee1ee7bb1f
SERNIKKI <badremilia@foxmail.com>
2021-04-12 20:44:00 +0800

    4/12

--- main/java/com/nikki/boot/controller/TableController.java
+++ main/java/com/nikki/boot/controller/TableController.java
@@ -1,13 +1,21 @@
 package com.nikki.boot.controller;
 
+import com.nikki.boot.bean.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 //处理有关table页面的请求
 @Controller
 public class TableController {
     @GetMapping("/basic_table")
-    public String basic_table(){
+    public String basic_table(Model model){
+        List<User> users = Arrays.asList(new User("zhangsan", 18), new User("lisi", 17),
+                new User("wangwu", 20));
+        model.addAttribute("users",users);
         return "table/basic_table";
     }
     @GetMapping("/dynamic_table")
